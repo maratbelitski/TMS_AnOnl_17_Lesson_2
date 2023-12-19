0c79735c8566e27a78241426849cf4c3927cc56f
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-19 13:06:40 +0300

    third task

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,14 @@ public class Main {
         System.out.print("Введите имя пользователя: ");
         System.out.println("Hello " + scanner.nextLine() + "!");
         System.out.println();
+
+        //Задача №3
+        System.out.println("Считаем сумму двух чисел.");
+        System.out.print("Введите первое число: ");
+        int numberA = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int numberB = scanner.nextInt();
+        System.out.println("Сумма чисел " + numberA + " и " + numberB + " равна " + (numberA + numberB));
     }
 }
