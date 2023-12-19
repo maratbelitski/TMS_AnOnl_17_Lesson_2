26a9cbe33dffe0c8bf4992e31a863b75ee287c3c
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-19 13:05:56 +0300

    second task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Belitski Marat
  * @date 19.12.2023
@@ -5,9 +7,15 @@
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         //Задача №1
         System.out.println("Hello world!");
+        System.out.println();
+
+        //Задача №2
+        System.out.print("Введите имя пользователя: ");
+        System.out.println("Hello " + scanner.nextLine() + "!");
         System.out.println();
     }
 }
